7f3d14bf3d9a401d123dc6ac68ad9523a5c57050
lsy969999 <lsy969999@gmail.com>
2024-01-23 03:19:01 +0900

    fc

---
+++ ios/wvwrapper/wvwrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  wvwrapperApp.swift
+//  wvwrapper
+//
+//  Created by SY L on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct wvwrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
